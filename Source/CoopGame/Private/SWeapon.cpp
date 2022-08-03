52bfabadd6e46fb5773285802d2a81ebd82086f5
tuna <gtuna52@gmail.com>
2022-08-03 10:52:32 +0300

    line tracing

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -3,6 +3,8 @@
 
 #include "SWeapon.h"
 
+#include "DrawDebugHelpers.h"
+
 // Sets default values
 ASWeapon::ASWeapon()
 {
@@ -17,6 +19,36 @@ ASWeapon::ASWeapon()
 void ASWeapon::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void ASWeapon::Fire()
+{
+	// trace the world, from pawn eyes to crosshair location
+
+	AActor* MyOwner = GetOwner();
+	if (MyOwner)
+	{
+		FVector EyeLocation;
+		FRotator EyeRotation;
+		MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+		FVector TraceEnd = EyeLocation + (EyeRotation.Vector() * 10000);
+
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(MyOwner);
+		QueryParams.AddIgnoredActor(this);
+		QueryParams.bTraceComplex = true;
+		
+		FHitResult HitResult;
+		bool bHitted = GetWorld()->LineTraceSingleByChannel(HitResult, EyeLocation, TraceEnd, ECC_Visibility, QueryParams );
+		if (bHitted)
+		{
+			//Blocking hit! Process damage
+			
+		}
+		DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1, 0,1);
+	}
+	
 }
 
 // Called every frame
