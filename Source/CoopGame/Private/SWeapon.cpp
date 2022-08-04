9c45e7366ac33eddc49eca5429e1ffc013112a9b
tuna <gtuna52@gmail.com>
2022-08-04 15:07:12 +0300

    console debug drawings macro

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -1,11 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "SWeapon.h"
 
 #include "DrawDebugHelpers.h"
 #include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystem.h"
+#include "Components/SkeletalMeshComponent.h"
 #include "Particles/ParticleSystemComponent.h"
+
+static int32 DebugWeaponDrawing = 0;
+FAutoConsoleVariableRef CVARDebugWeaponDrawing(
+TEXT("COOP.DebugWeapons"),
+DebugWeaponDrawing,
+TEXT("Draw Debug Lines for Weapons"),
+ECVF_Cheat);
 
 // Sets default values
 ASWeapon::ASWeapon()
@@ -64,7 +72,11 @@ void ASWeapon::Fire()
 			}
 			TracerEndPoint = HitResult.ImpactPoint;
 		}
-		DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1, 0,1);
+
+		if (DebugWeaponDrawing > 0)
+		{
+			DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1, 0,1);
+		}		
 
 		if (MuzzleEffect)
 		{
